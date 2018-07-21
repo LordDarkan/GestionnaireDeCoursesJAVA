d0c7351e19d9f7b535e56b5e55100d86b99e9cce
denis <denis-antoine@hotmail.be>
2018-07-21 15:23:44 +0200

    G>R

--- src/data/Mapper.java
+++ src/data/Mapper.java
@@ -1,22 +1,40 @@
 package data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.*;
+
+import models.Permanent;
 
 
 public class Mapper implements IMapper {
 	
 	private static final String PERSISTENCE_UNIT_NAME = "GestionnaireDeCourses";
-	private static EntityManagerFactory factory;
-	 
-	 public Mapper() {
-		 try{
-			 factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-			 EntityManager em = factory.createEntityManager();
+	private static EntityManagerFactory factory = null;
+	
+	public Mapper() {
+		 if(factory == null) {
+			try{
+				factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
 			}catch(Exception e) {
 				e.printStackTrace();
 				//System.exit(-1);
 			}
-		 
-	 }
-
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Permanent> getAllPermanent() {
+		List<Permanent> permanents = new ArrayList<>();
+		try{
+			EntityManager em = factory.createEntityManager();
+			Query q = em.createQuery("select t from Permanent t");
+			permanents = q.getResultList();
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return permanents;
+	}
 }
